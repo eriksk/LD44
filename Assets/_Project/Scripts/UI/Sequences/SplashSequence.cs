e15e0ce3c50ea24033442a4d9836b3efb5410fdc
skoggy <erikskoglund88@gmail.com>
2019-04-26 23:26:22 +0200

    added ldjam intro splash

---
+++ Assets/_Project/Scripts/UI/Sequences/SplashSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSequence : MonoBehaviour
+{
+    public AnimationClip Clip;
+    public string TargetSceneName;
+
+    void Start()
+    {
+        StartCoroutine(WaitForAnimationAndTransitionToScene());
+    }
+
+    private IEnumerator WaitForAnimationAndTransitionToScene()
+    {
+        yield return new WaitForSeconds(Clip.length);
+        SceneManager.LoadScene(TargetSceneName, LoadSceneMode.Single);
+    }
+}
